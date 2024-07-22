7004da6483f25046d40794a10f77f83dc297fd62
d-sapockij <asapotsk@bk.ru>
2024-07-22 23:59:19 +0700

    2 step

--- gendiff.js
+++ gendiff.js
@@ -6,6 +6,17 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.8.0');
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .version('0.8.0')
+  .option('-f, --format [type]', 'output format')
+  .parse();
 
-program.parse();
+// program.parse();
+
+
+// program
+//   .option('--no-sauce', 'Remove sauce')
+//   .option('--cheese <flavour>', 'cheese flavour', 'mozzarella')
+//   .option('--no-cheese', 'plain with no cheese')
+//   .parse();
