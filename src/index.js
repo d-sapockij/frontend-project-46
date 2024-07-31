5def9b047234abee6289f53442e69f17443866e5
d-sapockij <asapotsk@bk.ru>
2024-07-31 16:24:44 +0700

    edit index.js & stylish.js

--- src/index.js
+++ src/index.js
@@ -15,39 +15,39 @@ const compareObjects = (filepath1, filepath2) => {
     const keys = getUnique([...Object.keys(obj1), ...Object.keys(obj2)]);
     keys.forEach((key) => {
       if (!Object.hasOwn(obj1, key)) {
-        diffObj[`+ ${key}`] = obj2[key];
-        // diffObj[key] = {
-        //   name: key,
-        //   state:'added',
-        //   value: obj2[key],
-        // };
+        // diffObj[`+ ${key}`] = obj2[key];
+        diffObj[key] = {
+          name: key,
+          state:'added',
+          value: obj2[key],
+        };
       } else if (!Object.hasOwn(obj2, key)) {
-        diffObj[`- ${key}`] = obj1[key];
-        // diffObj[key] = {
-        //   name: key,
-        //   state:'deleted',
-        //   value: obj1[key],
-        // };
+        // diffObj[`- ${key}`] = obj1[key];
+        diffObj[key] = {
+          name: key,
+          state:'deleted',
+          value: obj1[key],
+        };
       } else if (obj1[key] !== obj2[key]) {
         if (typeof obj1[key] !== 'object' || typeof obj2[key] !== 'object') {
-          diffObj[`- ${key}`] = obj1[key];
-          diffObj[`+ ${key}`] = obj2[key];
-          // diffObj[key] = {
-          //   name: key,
-          //   state:'changed',
-          //   beforeValue: obj1[key],
-          //   afterValue: obj2[key],
-          // };
+          // diffObj[`- ${key}`] = obj1[key];
+          // diffObj[`+ ${key}`] = obj2[key];
+          diffObj[key] = {
+            name: key,
+            state:'changed',
+            beforeValue: obj1[key],
+            afterValue: obj2[key],
+          };
         } else {
           diffObj[`  ${key}`] = iter(obj1[key], obj2[key]);
         }
       } else {
-        diffObj[`  ${key}`] = obj1[key];
-        // diffObj[key] = {
-        //   name: key,
-        //   state:'unchanged',
-        //   value: obj1[key],
-        // };
+        // diffObj[`  ${key}`] = obj1[key];
+        diffObj[key] = {
+          name: key,
+          state:'unchanged',
+          value: obj1[key],
+        };
       }
     });
   
@@ -55,7 +55,7 @@ const compareObjects = (filepath1, filepath2) => {
   };
 
   const result = iter(obj1, obj2);
-  return stylish(result, '.', 4)
+  return stylish(result, ' ', 4)
   // return JSON.stringify(result, null, '   ').replaceAll('"', '').replaceAll(',', '');
 };
 
--- src/stylish.js
+++ src/stylish.js
@@ -2,26 +2,26 @@ const stringify = (obj) => JSON.stringify(obj, null, ' ').replaceAll('"', '');
 
 const stylish = (obj, replacer = ' ', spacesCount = 1) => {
     const iter = (currentValue, depth) => {
-      // if (Object.hasOwn(currentValue, 'state')) {
-      //   if (currentValue.state === 'added') {
-      //       return `+ ${currentValue.name}: ${stringify(currentValue.value)}`;
-      //   }
-      //   if (currentValue.state === 'deleted') {
-      //       return `- ${currentValue.name}: ${stringify(currentValue.value)}`;
-      //   }
-      //   if (currentValue.state === 'changed') {
-      //       return (
-      //       `- ${currentValue.name}: ${stringify(currentValue.beforeValue)} 
-      //        + ${currentValue.name}: ${stringify(currentValue.afterValue)}`
-      //     );
-      //   }
-      //   if (currentValue.state === 'unchanged') {
-      //       return `  ${currentValue.name}: ${stringify(currentValue.value)}`;
-      //   }
-    // }
-    if (typeof currentValue !== 'object' || currentValue === null) {
-      return `${currentValue}`;
+      if (Object.hasOwn(currentValue, 'state')) {
+        if (currentValue.state === 'added') {
+            return `+ ${currentValue.name}: ${stringify(currentValue.value)}`;
+        }
+        if (currentValue.state === 'deleted') {
+            return `- ${currentValue.name}: ${stringify(currentValue.value)}`;
+        }
+        if (currentValue.state === 'changed') {
+            return (
+            `- ${currentValue.name}: ${stringify(currentValue.beforeValue)} 
+             + ${currentValue.name}: ${stringify(currentValue.afterValue)}`
+          );
+        }
+        if (currentValue.state === 'unchanged') {
+            return `  ${currentValue.name}: ${stringify(currentValue.value)}`;
+        }
     }
+      if (typeof currentValue !== 'object' || currentValue === null) {
+        return `${currentValue}`;
+      }
   
       const indentSize = depth * spacesCount;
       const currentIndent = replacer.repeat(indentSize - 2);
@@ -36,7 +36,27 @@ const stylish = (obj, replacer = ' ', spacesCount = 1) => {
         ...lines,
         `${bracketIndent}}`,
       ].join('\n');
+
+    // if (Object.hasOwn(currentValue, 'name') && Object.hasOwn(currentValue, 'state')) {
+    //   return currentValue;
+    // }
+
+    // const indentSize = depth * spacesCount;
+    // const currentIndent = replacer.repeat(indentSize);
+    // const bracketIndent = replacer.repeat(indentSize - spacesCount);
+    // const lines = Object
+    //   .entries(currentValue)
+    //   .map(([key, val]) => `${currentIndent}${key}: ${iter(val, depth + 1)}`);
+
+    // const keys = Object.keys(currentValue);
+    // keys.forEach((key) => {
+      // if (currentValue.state === 'added') {
+      //   `+ ${currentValue.name}: ${stringify(currentValue.value)}`;
+      // }
+    // })
+
     };
+
   
     return iter(obj, 1);
   };
